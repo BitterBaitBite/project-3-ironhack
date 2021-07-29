d8072a1279c798bdd114995834ab75da90066c14
Bitterbaitbite <guillechernandez@gmail.com>
2021-07-29 16:34:32 +0200

    deploy

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,3 @@
-// module.exports = app => {
-// 	app.use('/api', require('./auth.routes.js'));
-// 	// app.use('/api/users', require('./user.routes.js'));
-// 	// app.use('/api/portfolio', require('./portfolio.routes.js'));
-// 	// app.use('/api/job-offers', require('./jobOffers.routes.js'));
-// };
-
 module.exports = app => {
 	app.use('/api', require('./auth.routes.js'));
 	app.use('/api/users', require('./user.routes.js'));
